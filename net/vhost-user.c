amorenoz/qemu

commit c6beefd674fff8d41b90365dfccad32e53a5abcb

vhost-user: save features if the char dev is closed
That way the state can be correctly restored when the device is opened
again. This might happen if the backend is restarted.

Buglink: https://bugzilla.redhat.com/show_bug.cgi?id=1738768
Reported-by: Pei Zhang <pezhang@redhat.com>
Fixes: 6ab79a20af3a ("do not call vhost_net_cleanup() on running net from char user event")
Cc: ddstreet@canonical.com
Cc: Michael S. Tsirkin <mst@redhat.com>
Cc: qemu-stable@nongnu.org
Signed-off-by: Adrian Moreno <amorenoz@redhat.com>
Message-Id: <20190924162044.11414-1-amorenoz@redhat.com>
Acked-by: Jason Wang <jasowang@redhat.com>
Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

net/vhost-user.c
@@ -235,6 +235,10 @@ static void chr_closed_bh(void *opaque)
 
     s = DO_UPCAST(NetVhostUserState, nc, ncs[0]);
 
+    if (s->vhost_net) {
+        s->acked_features = vhost_net_get_acked_features(s->vhost_net);
+    }
+
     qmp_set_link(name, false, &err);
 
     qemu_chr_fe_set_handlers(&s->chr, NULL, NULL, net_vhost_user_event,

commit 2ab0a94a60006def40e798d843b8c6791081818a

vhost-user: Save acked_features for all the queues
Currently only the acked_features of the first queue pair is saved. Do
so for all the queues to ensure they all come up well on reconnection.

Fixes: c6beefd674ff("vhost-user: save features if the char dev is
closed")

Signed-off-by: Xuan Ding <xuan.ding@intel.com>
Signed-off-by: Adrian Moreno <amorenoz@redhat.com>

net/vhost-user.c
@@ -45,6 +45,22 @@ uint64_t vhost_user_get_acked_features(NetClientState *nc)
     return s->acked_features;
 }
 
+static void vhost_user_save_acked_features(int queues, NetClientState *ncs[])
+{
+    NetVhostUserState *s;
+    int i;
+
+    for (i = 0; i < queues; i++) {
+        assert(ncs[i]->info->type == NET_CLIENT_DRIVER_VHOST_USER);
+
+        s = DO_UPCAST(NetVhostUserState, nc, ncs[i]);
+
+        if (s->vhost_net) {
+            s->acked_features = vhost_net_get_acked_features(s->vhost_net);
+        }
+    }
+}
+
 static void vhost_user_stop(int queues, NetClientState *ncs[])
 {
     NetVhostUserState *s;
@@ -235,9 +251,7 @@ static void chr_closed_bh(void *opaque)
 
     s = DO_UPCAST(NetVhostUserState, nc, ncs[0]);
 
-    if (s->vhost_net) {
-        s->acked_features = vhost_net_get_acked_features(s->vhost_net);
-    }
+    vhost_user_save_acked_features(queues, ncs);
 
     qmp_set_link(name, false, &err);
 